4e57365ecf64a353f5c5bac8409d2f350eeb8c8f
namoo36 <namoo36@namoo36ui-MacBookPro.local>
2025-10-15 16:38:28 +0900

    Initial commit

---
+++ src/main/java/com/space/munova/MunovaApplication.java
@@ -0,0 +1,13 @@
+package com.space.munova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunovaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/space/munova/MunovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.munova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

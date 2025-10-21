513598d39a5781425973835578dc784376ef3f65
jeondoh <jundo1302@gmail.com>
2025-10-21 13:03:54 +0900

    fix: 클래스명 변경

--- src/main/java/com/space/munova/core/entity/BaseEntity.java
+++ src/main/java/com/space/munova/core/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.space.munova.core.config;
+package com.space.munova.core.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
@@ -10,14 +10,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-/// 해당 클래스 상속 받을경우
-/// 상속받은 엔터티 테이블은 createAt updatedAt이 생기고 자동으로 업데이트된다
-/// 메인 클래스 상단에 @EnableJpaAuditing 추가
-@EntityListeners(AuditingEntityListener.class)
-///  공통 매핑 정보가 필요할때 속성만 받아 사용할 수 있게하는 어노테이션
-@MappedSuperclass
 @Getter
-public class TimeBaseEntity {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
@@ -25,5 +21,4 @@ public class TimeBaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
 }

bb54122e2e2330c5b4b0167bbad6e7bb39f53db2
ximvamom <yoonji0624@naver.com>
2025-10-21 16:03:10 +0900

    [MUNOVA-16] :sparkles: feat: 주문 생성 기능 구현
    # Conflicts:
    #	src/main/java/com/space/munova/member/repository/MemberRepository.java

--- src/main/java/com/space/munova/member/entity/Member.java
+++ src/main/java/com/space/munova/member/entity/Member.java
@@ -14,7 +14,7 @@ public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+//    @Column(name = "member_id")
     private Long id;
 
     private String username;

e9eea66a770839975e9583667aafa7dbd4801aec
김성현 <mydannykim@gmail.com>
2025-10-22 10:30:12 +0900

    refactor: api controller 수정 (user,seller,admin 세분화)/ 2025.10.22/ 김성현

--- src/main/java/com/space/munova/recommend/controller/CommonController.java
+++ src/main/java/com/space/munova/recommend/controller/CommonController.java
@@ -8,30 +8,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recommend")
 @RequiredArgsConstructor
 public class CommonController {
 
     private final RecommendService recommendService;
 
-    @PutMapping("/{userId}/{productId}")
+    @PutMapping("/recommend/{userId}/{productId}")
     public String updateMemberProductRecommend(@PathVariable Long userId, @PathVariable Long productId) {
         recommendService.updateUserProductRecommend(userId, productId);
         return "User recommendation updated successfully";
     }
 
-    @PutMapping("/{productId}")
+    @PutMapping("/recommend/{productId}")
     public String updateSimilarProductRecommend(@PathVariable Long productId) {
         recommendService.updateSimilarProductRecommend(productId);
         return "Similar product recommendation updated successfully";
     }
 
-    @GetMapping("/user/{userId}/product/{productId}/based_on")
+    @GetMapping("/api/admin/recommend/user/{userId}/product/{productId}/based_on")
     public List<ResponseDto> getRecommendationReason(@PathVariable Long userId, @PathVariable Long productId) {
         return recommendService.getRecommendationReason(userId, productId);
     }
 
-    @GetMapping("/user/{userId}/product/{productId}/score")
+    @GetMapping("/api/admin/recommend/user/{userId}/product/{productId}/score")
     public double getRecommendationScore(@PathVariable Long userId, @PathVariable Long productId) {
         return recommendService.getRecommendationScore(userId, productId);
     }
--- src/main/java/com/space/munova/recommend/controller/ProductController.java
+++ src/main/java/com/space/munova/recommend/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recommend/products")
+@RequestMapping("/api/admin/recommend/products")
 @RequiredArgsConstructor
 public class ProductController {
 

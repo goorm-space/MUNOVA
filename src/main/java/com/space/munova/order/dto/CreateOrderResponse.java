e0f0556301a9ad39c829529e73fac2cb0bf6335e
ximvamom <yoonji0624@naver.com>
2025-10-23 18:23:51 +0900

    [MUNOVA-66] :recycle: refactor: 예외처리 및 주문 관련 코드 리팩토링

--- src/main/java/com/space/munova/order/dto/CreateOrderResponse.java
+++ src/main/java/com/space/munova/order/dto/CreateOrderResponse.java
@@ -5,12 +5,19 @@ import com.space.munova.order.entity.Order;
 public record CreateOrderResponse(
     Long orderId,
     String orderNum,
+    String userName,
+    String address,
+    String userRequest,
     Long totalPrice
+    // Todo: 결제 수단 가져오자
 ) {
     public static CreateOrderResponse from(Order order) {
         return new CreateOrderResponse(
                 order.getId(),
                 order.getOrderNum(),
+                order.getMember().getUsername(),
+                order.getMember().getAddress(),
+                order.getUserRequest(),
                 order.getTotalPrice()
         );
     }

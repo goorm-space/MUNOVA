15985d1174a107993dfcfee3d0c4c843b5bfaa40
jeondoh <jundo1302@gmail.com>
2025-10-28 01:25:19 +0900

    [MUNOVA-92] refactor: 쿠폰 사용 리팩토링

--- src/main/java/com/space/munova/coupon/service/CouponService.java
+++ src/main/java/com/space/munova/coupon/service/CouponService.java
@@ -7,13 +7,10 @@ import com.space.munova.coupon.dto.UseCouponResponse;
 
 public interface CouponService {
 
-    /**
-     * 쿠폰 발급
-     */
+
+    // 쿠폰 발급
     IssueCouponResponse issueCoupon(IssueCouponRequest issueCouponRequest);
 
-    /**
-     * 쿠폰 사용
-     */
+    // 쿠폰 사용
     UseCouponResponse useCoupon(Long couponId, UseCouponRequest useCouponRequest);
 }

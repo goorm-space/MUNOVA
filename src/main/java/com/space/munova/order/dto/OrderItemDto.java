6106e2622e53d03c832714fc142732ba9eb9a7af
ximvamom <yoonji0624@naver.com>
2025-10-31 02:35:52 +0900

    [MUNOVA-134] :recycle: refactor: 옵션 정보 불러오기

--- src/main/java/com/space/munova/order/dto/OrderItemDto.java
+++ src/main/java/com/space/munova/order/dto/OrderItemDto.java
@@ -2,21 +2,41 @@ package com.space.munova.order.dto;
 
 import com.space.munova.order.entity.OrderItem;
 
+import java.util.stream.Collectors;
+
 public record OrderItemDto(
         Long orderItemId,
+        String brandName,
         String productName,
-        // Todo: 판매자, 옵션정보, 이미지도 불러와야한다
-        int quantity,
+        String option,
+        Integer quantity,
         Long totalPrice,
-        OrderStatus status
+        OrderStatus status,
+        String imageUrl
 ) {
     public static OrderItemDto from(OrderItem orderItem) {
+        String optionStr = OptionDto.combineOptionNamesByType(
+                orderItem.getProductDetail()
+                        .getOptionMappings()
+                        .stream()
+                        .map(mapping -> new OptionDto(
+                                mapping.getOption().getId(),
+                                mapping.getOption().getOptionType(),
+                                mapping.getOption().getOptionName()
+                        ))
+                        .collect(Collectors.toList())
+        );
+
         return new OrderItemDto(
                 orderItem.getId(),
+                orderItem.getProductDetail().getProduct().getBrand().getBrandName(),
                 orderItem.getNameSnapshot(),
+                optionStr,
                 orderItem.getQuantity(),
-                orderItem.getPriceSnapshot() * orderItem.getQuantity(),
-                orderItem.getStatus()
+                orderItem.getPriceSnapshot(),
+                orderItem.getStatus(),
+"https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcREdixMliFliUNy1jx3BOcvILQ-NYjVbkF63dVbnTFLybgPvHMHqSaUP5M7ur6G--T5KngDwZVTZt3w4DcFUeb-crAWVWic2h92FAIlg8DHvuOLTvOrKaEQdzH_cwlqpG3nqoU20VE&usqp=CAc"
+//                orderItem.getProductDetail().getProduct().getProductImages().getFirst().getOriginName()
         );
     }
 }

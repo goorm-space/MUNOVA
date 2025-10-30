4a1973a66a9dcf6e54090f3a610f54e854a41e8f
namoo36 <wldnrkwhr36@naver.com>
2025-10-30 15:50:21 +0900

    [MUNOVA-115] :sparkles: feat: S3

--- src/main/java/com/space/munova/chat/dto/group/MemberInfoDto.java
+++ src/main/java/com/space/munova/chat/dto/group/MemberInfoDto.java
@@ -1,10 +1,14 @@
 package com.space.munova.chat.dto.group;
 
+import com.space.munova.chat.enums.ChatUserType;
+
 public record MemberInfoDto(
         Long memberId,
-        String name
+        String name,
+        ChatUserType chatUserType
+
 ) {
-    public static MemberInfoDto of(Long memberId, String name) {
-        return new MemberInfoDto(memberId, name);
+    public static MemberInfoDto of(Long memberId, String name, ChatUserType chatUserType) {
+        return new MemberInfoDto(memberId, name, chatUserType);
     }
 }
